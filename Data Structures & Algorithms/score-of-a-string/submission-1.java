e2885471ef6a4add4efee0ea45e488d7276aca9c
Jayesh Jain <jayeshjain32@gmail.com>
2026-08-01 19:35:52 +0530

    Add: score-of-a-string - submission-1

---
+++ Data Structures & Algorithms/score-of-a-string/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    int sum = 0;
+    public int scoreOfString(String s) {
+        for(int i=0; i < s.length()-1; i++){
+            int val1 = s.charAt(i);
+            int val2 = s.charAt(i+1);
+            sum += (val1 > val2) ? (val1 - val2) : (val2 - val1);
+        }
+        return sum;
+    }
+}
