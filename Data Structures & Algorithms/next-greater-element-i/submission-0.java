df9eaf23904ee09dbdf580ca623b01aa78a9ee7c
Jayesh Jain <jayeshjain32@gmail.com>
2026-08-10 21:53:46 +0530

    Add: next-greater-element-i - submission-0

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        List<Integer> n1List = Arrays.stream(nums1)
+                                .boxed()
+                                .collect(Collectors.toCollection(ArrayList::new));
+        List<Integer> n2List = Arrays.stream(nums2)
+                                .boxed()
+                                .collect(Collectors.toCollection(ArrayList::new));
+        List<Integer> result = new ArrayList<>();
+
+        for(int i = 0; i < n1List.size(); i++){
+            int n2Index = n2List.indexOf(n1List.get(i));
+            if(n2List.contains(n1List.get(i)) && (n2Index + 1) < n2List.size() - 1){
+                if(n2List.get(n2Index + 1) > n1List.get(i)){
+                    result.add(n2List.get(n2Index + 1));
+                } else{
+                    result.add(-1);    
+                }
+            } else{
+                result.add(-1);
+            }
+        }
+        return result.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
