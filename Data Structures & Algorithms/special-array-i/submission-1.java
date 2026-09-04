25f94426b3b8eb079e64e6ccde2f113573cc89a3
Jayesh Jain <jayeshjain32@gmail.com>
2026-09-04 22:33:25 +0530

    Add: special-array-i - submission-1

---
+++ Data Structures & Algorithms/special-array-i/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] % 2 == nums[i] % 2) {
+                return false;
+            }
+        }
+        return true;
+        // My Solution
+        /*if(nums.length == 1){
+            return true;
+        }
+        boolean flag = false;
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] % 2 == 0 && nums[i + 1] % 2 !=0){
+                flag = true;
+            } else if(nums[i] % 2 != 0 && nums[i + 1] % 2 ==0){
+                flag = true;
+            } else {
+                flag = false;
+                break;
+            }
+        }
+
+        return flag;*/
+    }
+}
