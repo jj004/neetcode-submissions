00c748a228a5565790bb052d103fadb7e3193193
Jayesh Jain <jayeshjain32@gmail.com>
2026-09-11 12:21:29 +0530

    Add: evaluate-reverse-polish-notation - submission-6

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-6.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> data = new Stack<>();
+        int num1 = 0;
+        int num2 = 0;
+        for(String op : tokens){
+            switch(op){
+                case "+":
+                    num2 = data.pop();
+                    num1 = data.pop();
+                    data.push(num1 + num2);
+                    break;
+                case "-":
+                    num2 = data.pop();
+                    num1 = data.pop();
+                    data.push(num1 - num1);
+                    break;
+                case "*":
+                    num2 = data.pop();
+                    num1 = data.pop();
+                    data.push(num1 * num2);
+                    break;
+                case "/":
+                    num2 = data.pop();
+                    num1 = data.pop();
+                    data.push(num1 / num2);
+                    break;
+                default:
+                    data.push(Integer.parseInt(op));
+                    break;
+            }
+        }
+
+        return data.pop();
+    }
+}
