c3f2dd9614ea32654b0c8ec1f5b4a51236b0cce1
Jayesh Jain <jayeshjain32@gmail.com>
2026-09-16 20:48:09 +0530

    Add: basic-calculator-ii - submission-2

---
+++ Data Structures & Algorithms/basic-calculator-ii/submission-2.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> val = new Stack<>();
+        Stack<Character> op = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(Character.isDigit(ch)){
+                StringBuilder sb = new StringBuilder();
+                while(i < s.length() && Character.isDigit(s.charAt(i))){
+                    sb.append(s.charAt(i));
+                    i++;
+                }
+                val.push(Integer.parseInt(sb.toString()));
+                i--;
+            } else if(ch == '*' || ch == '/' || ch == '+' || ch == '-'){
+                while(!op.isEmpty() && this.hasPrecedence(ch, op.peek())){
+                    val.push(this.applyOperation(ch, val.pop(), val.pop()));
+                }
+                op.push(ch);
+            }
+        }
+
+        while(!op.isEmpty()){
+            val.push(this.applyOperation(op.pop(), val.pop(), val.pop()));
+        }
+
+        return val.pop();
+    }
+
+    private boolean hasPrecedence(char op1, char op2){
+        return (op1 != '*' && op1 != '/') || (op2 != '+' && op2 != '-');
+    }
+
+    private int applyOperation(char op, int b, int a){
+        return switch (op) {
+            case '+' -> a + b;
+            case '-' -> a - b;
+            case '*' -> a * b;
+            case '/' -> a / b;
+            default -> 0;
+        };
+
+    }
+}
