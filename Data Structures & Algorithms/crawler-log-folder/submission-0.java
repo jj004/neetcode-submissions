d782f99333b8b037c9461bf6e5603afbf77b2f96
Jayesh Jain <jayeshjain32@gmail.com>
2026-09-20 15:15:00 +0530

    Add: crawler-log-folder - submission-0

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        String main = logs[0];
+        Stack<String> st = new Stack<>();
+        int count = 0;
+
+        for(String folder : logs){
+            if(!st.isEmpty() && folder.equals("../")){
+                st.pop();
+            }
+            if(folder.equals("./")){
+                continue;
+            }
+            st.push(folder);
+        }
+
+        while(!st.isEmpty() && !main.equals(st.peek())){
+            st.pop();
+            count++;
+        }
+
+        return count;
+    }
+}
